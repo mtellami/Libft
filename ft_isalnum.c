835424a4a28b4412cfff0fe16909d621072b6e8e
Moadtell <moadtell@gmail.com>
2022-09-01 18:24:57 +0100

    norminette

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,20 +12,6 @@
 
 #include "libft.h"
 
-int	ft_isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
-
-int	ft_isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	return (0);
-}
-
 int	ft_isalnum(int c)
 {
 	if (ft_isdigit(c) || ft_isalpha(c))
